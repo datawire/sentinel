3e6dc9897039be5c6494a7bad66964a151fe0b40
Philip Lombardi <plombardi@datawire.io>
2016-09-12 14:31:21 -0400

    Ngrok utility script

---
+++ bin/ngrok.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# ngrok.sh
+#
+# Start an ngrok tunnel which is useful for sharing a local service among developers on a team. This script assumes
+# that you have an authorized ngrok account that can assign a subdomain.
+
+SERVICE_NAME="$(cat Datawirefile | python -c 'import sys, json; print json.load(sys.stdin)["service"]["name"]')"
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+SUBDOMAIN="${SERVICE_NAME}-$(whoami)-${GIT_BRANCH}"
+
+printf "Starting ngrok.io tunnel... (subdomain: ${SUBDOMAIN})\n"
+ngrok http -subdomain "$SUBDOMAIN" 5000
